13b19d9a6e6d5b69f19206c44c5d2b5e4fabe947
Mariama Koulibaly <mariama.koulibaly.587@gmail.com>
2026-05-22 21:24:00 +0100

    config render + docker

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
             'prefix'         => '',
             'prefix_indexes' => true,
             'search_path'    => 'public',
-            'sslmode'        => 'require',
+            'sslmode'        => 'disable',
         ],
 
     ],
